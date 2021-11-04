c055f06694222076db6aeceb27ad48936716c503
Edwike12 <nyaunchoedwike@gmail.com>
2021-11-04 17:10:48 +0300

    save all changes and added a contact us form using mailchimp

--- js/script.js
+++ js/script.js
@@ -1,30 +1,7 @@
-$(function () {
-    let desp = $('#desp');
-    let devp = $('#devp');
-    let prop = $('#prop');
-    let destext = $('#destxt');
-    let devtext = $('#devtext');
-    let protext = $('#protext');
 
-    $(desp).hide();
-    $(devp).hide();
-    $(prop).hide();
-});
-
- //Show paragraph content
- $('.des').on('click', function () {
-    $('.des').slideUp('slow');
-    $(desp).show(500);
-    $(destext).animate({fontWeight:'bolder',fontSize:'30px'})
- });
- $('.dev').on('click', function () {
-    $('.dev').slideUp('slow');
-    $(devp).show(500);
-    $(devtext).animate({fontWeight:'bolder',fontSize:'30px'})
-});
-$('.pro').on('click', function () {
-    $('.pro').slideUp('slow');
-    $(prop).show(500);
-    $(protext).animate({fontWeight:'bolder',fontSize:'30px'})
-});
-
+$(document).ready(function() {
+    $(".clickable").click(function() {
+      $(".design-showing").toggle();
+      $(".design-details").toggle();
+    });
+  });
