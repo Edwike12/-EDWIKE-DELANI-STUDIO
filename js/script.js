9835899e50d4394dfa50fa9f9be2f8d8e1fcaa23
Edwike12 <nyaunchoedwike@gmail.com>
2021-11-06 21:45:42 +0300

    changes on html,style and script

--- js/script.js
+++ js/script.js
@@ -1,7 +1,28 @@
-
 $(document).ready(function() {
-    $(".clickable").click(function() {
-      $(".design-showing").toggle();
-      $(".design-details").toggle();
-    });
+    // $(".clickable").click(function() {
+    //   $("image").toggle();
+    //   $("p").toggle();
+    // });
+
+  
+    
   });
+  const Name = {
+    Name: function() {
+      return this.Name + " " + this.Message;
+    }
+  }
+  const Email = {
+    Email =""
+  }
+  const Message = {
+    Message:"  we have received your message.Thank you for contacting us",
+  }
+   
+  var element=document.getElementById("design");
+  var listener=element.addEventListener('click',function(event){   
+
+  }); 
+
+  document.getElementById("demo").innerHTML = form.Name.call();
+
