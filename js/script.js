ddb608e4f27cbf36d586900353b9223fb5957b8b
Edwike12 <nyaunchoedwike@gmail.com>
2021-11-08 11:20:14 +0300

    add all changes

--- js/script.js
+++ js/script.js
@@ -1,4 +1,46 @@
 
+     document.getElementById("design_showing").addEventListener(
+       "click",
+        function () {
+          //   document.getElementById("design_details").style.display = "block";
+          if (
+            document.getElementById("design_details").style.display == "none"
+          ) {
+            document.getElementById("design_details").style.display = "block";
+         } else {
+          document.getElementById("design_details").style.display = "none";
+         }
+        },
+        false
+      ); 
+      document.getElementById("development_showing").addEventListener(
+       "click",
+        function () {
+          //   document.getElementById("design_details").style.display = "block";
+          if (
+            document.getElementById("development_details").style.display == "none"
+          ) {
+            document.getElementById("development_details").style.display = "block";
+         } else {
+          document.getElementById("development_details").style.display = "none";
+         }
+        },
+        false
+      ); 
+      document.getElementById("product_showing").addEventListener(
+       "click",
+        function () {
+          //   document.getElementById("design_details").style.display = "block";
+          if (
+            document.getElementById("product_details").style.display == "none"
+          ) {
+            document.getElementById("product_details").style.display = "block";
+         } else {
+          document.getElementById("product_details").style.display = "none";
+         }
+        },
+        false
+      );
   const Name = {
     Name: function() {
       return this.Name + " " + this.Message;
